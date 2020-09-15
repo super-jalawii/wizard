b564a7c95acc952c1cfd5fc819079e03fd72b17b
super-jalawii <super-jalawii@protonmail.com>
2020-09-15 00:20:47 -0400

    Adding event system

---
+++ event.rkt
@@ -0,0 +1,76 @@
+#lang racket
+
+(require racket/generic
+         racket/match
+         (for-syntax racket/syntax
+                     syntax/parse))
+
+(provide define-event
+         send
+         process)
+
+(define-generics event
+  (event-type event))
+
+(define events (make-hash))
+
+(define-syntax [define-event stx]
+  (syntax-parse stx
+    [(_ events:expr name:id ([type:id params:id ...] ...+))
+     #`(begin
+         (hash-set! events (quote name) '())
+         #,@(for/list ([t  (in-list (syntax-e #'(type ...)))]
+                       [ps (in-list (syntax-e #'((params ...) ...)))])
+              (with-syntax ([struct-id (format-id t "~a::~a" #'name t)])
+                #`(struct #,(datum->syntax t #'struct-id) #,ps
+                    #:transparent
+                    #:methods gen:event
+                    [(define [event-type self] (quote name))]))))]))
+
+(define [send events msg]
+  (hash-set! events
+             (event-type msg)
+             (cons msg (hash-ref events (event-type msg)))))
+
+(define-syntax [read-events stx]
+  (syntax-parse stx
+    [(_ events:expr type:id)
+     #'(hash-ref events (quote type))]))
+
+;; ;;; Things to figure out
+
+;; ;; Events - publish messages - subscribe to messages. Each subscriber reads the
+;; ;; message one time, then it is deleted.
+
+;; FIXME: Figure out how to represent the "else" branch
+(define-syntax [process stx]
+  (syntax-parse stx
+    [(_ events:expr name:id
+        [(type:id params:id ...) body ...+] ...+)
+     #'(for/list ([msg (read-events events name)])
+         (match msg
+           [(type params ...) body ...]
+           ...))]))
+
+;; Some test data to make sure things are working alright
+(define-event events Move ([Teleport ent x y]
+                           [Adjacent ent x y]))
+
+(send events (Move::Teleport 1 100 100))
+(send events (Move::Adjacent 1  10  10))
+(send events (Move::Teleport 2  15  40))
+
+(process events Move
+          [(Move::Teleport ent x y)
+           (printf "Ent ~a teleported to ~a ~a\n" ent x y)]
+          [(Move::Adjacent ent x y)
+           (printf "Ent ~a moved to ~a ~a\n" ent x y)])
+
+;; TODO: Can we treat messages as another type of entity storage? So that we
+;; can join to receive only messages for entities which satisfy the criteria (in
+;; terms of components present?)
+
+
+
+
+
