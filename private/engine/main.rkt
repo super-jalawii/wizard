21ba6415817c0411d51aa3e71dc6037ceeee81ff
super-jalawii <super-jalawii@protonmail.com>
2020-09-29 17:17:30 -0400

    Refactored main game loop to support animation system and fullscreen

--- private/engine/main.rkt
+++ private/engine/main.rkt
@@ -2,51 +2,69 @@
 
 (require raylib
          "state.rkt"
+         "../config.rkt"
          wizard/ecs)
 
 (provide (rename-out [make-Window Window])
+         *delta-time*
          run)
 
-(struct Window (width height title fullscreen show-logo))
+(struct Window (width height title fullscreen))
 
-(define [make-Window width height title
-                     #:fullscreen [fullscreen #f]
-                     #:show-logo [show-logo #f]]
-  (Window width height title fullscreen show-logo))
+(define (make-Window width height title #:fullscreen [fullscreen #f])
+  (Window width height title fullscreen))
 
-(define [Window-init win]
-  (init-window (Window-width  win)
-               (Window-height win)
-               (Window-title  win)))
+(define (Window-init win)
+  (if (Window-fullscreen win)
+      (begin
+        (printf "Fullscreen enabled")
+        (set-config-flags! 'FLAG_FULLSCREEN_MODE)
+        (init-window (screen-width)
+                     (screen-height)
+                     (Window-title win)))
+      (begin
+        (printf "Fullscreen disabled")
+        (set-config-flags! 'FLAG_NONE)
+        (init-window (Window-width  win)
+                     (Window-height win)
+                     (Window-title  win)))))
+
+(define Window-close close-window)
 
 (define *gsm* (make-parameter (GameStateManager)))
+(define *delta-time* (make-parameter 0))
 
-(define [gameloop ticks-per-second max-frameskip]
-  (define ticks-per-milli (ticks-per-second . / . 1000))
-  (define skip-ticks ticks-per-milli)
-  (let outer-loop ([next-game-tick (elapsed-time)])
-    (unless (window-should-close?)
-      (gsm:handle-input (*gsm*))
+(define (gameloop)
+  (let* ([ticks-per-second (config:get 'ticks-per-second)]
+         [max-frameskip    (config:get 'max-frameskip)   ]
+         [ticks-per-milli  (ticks-per-second . / . 1000) ]
+         [skip-ticks       ticks-per-milli               ]
+         [last-frame       (elapsed-time)                ])
+    (let outer-loop ([next-game-tick (elapsed-time)])
+      (unless (window-should-close?)
+        (parameterize ([*delta-time* (* 1000 (- (elapsed-time) last-frame))])
+          (set! last-frame (elapsed-time))
+          (gsm:handle-input (*gsm*))
 
-      (let gamestep ([step 0])
-        (when (and (> (elapsed-time) next-game-tick)
-                   (< step max-frameskip))
+          (let gamestep ([step 0])
+            (when (and (> (elapsed-time) next-game-tick)
+                       (< step max-frameskip))
 
-          (gsm:update (*gsm*))
+              (gsm:update (*gsm*))
 
-          (set! next-game-tick (+ next-game-tick skip-ticks))
-          (gamestep (add1 step))))
+              (set! next-game-tick (+ next-game-tick skip-ticks))
+              (gamestep (add1 step))))
 
-      (gsm:draw (*gsm*))
+          (gsm:draw (*gsm*))
 
-      (outer-loop next-game-tick))))
+          (outer-loop next-game-tick))))))
 
-(define [run win
-             #:init-gs [init-gs #f]
-             #:ticks-per-second [ticks-per-second 60]
-             #:max-frameskip [max-frameskip 10]]
-  (parameterize ([*gsm* (GameStateManager (list init-gs))]
+;; FIXME: Run is essentially implementing its own gamestate (where the window is
+;; initialized, and closed). Can we make this work like a gamestate too?
+(define (run window initial-gamestate)
+  (parameterize ([*gsm* (GameStateManager (list initial-gamestate))]
                  #;[*world* (make-hash)]
                  [*next-eid* 0])
-    (Window-init win)
-    (gameloop ticks-per-second max-frameskip)))
+    (Window-init window)
+    (gameloop)
+    (Window-close)))
